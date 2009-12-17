b35713862e55ff5e39fc1d1de33d4795c2a9e3dd
www.ehust <www.ehust@gmail.com>
2009-12-17 00:26:40 +0000

    忘了

--- src/types.h
+++ src/types.h
@@ -4,7 +4,7 @@ typedef unsigned char *POINTER;
 typedef unsigned char BYTE;
 typedef unsigned char UCHAR;
 typedef unsigned short int WORD;
-typedef long LONG;
+typedef int LONG;
 typedef unsigned int DWORD;
 typedef unsigned int UINT4;
 #endif
